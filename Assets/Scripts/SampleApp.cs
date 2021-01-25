b59dd5f7cc9451ca65bf5a15593941e5a49e3928
Timothy Graupmann <tim.graupmann@razer.com>
2021-01-25 11:02:45 -0800

    Add appinfo category info

--- Assets/Scripts/SampleApp.cs
+++ Assets/Scripts/SampleApp.cs
@@ -26,8 +26,9 @@ public class SampleApp : MonoBehaviour
         //    0x08 | // Mousepads
         //    0x10 | // Keypads
         //    0x20   // ChromaLink devices
-        //    ;
         appInfo.SupportedDevice = (0x01 | 0x02 | 0x04 | 0x08 | 0x10 | 0x20);
+        //    0x01 | // Utility. (To specifiy this is an utility application)
+        //    0x02   // Game. (To specifiy this is a game);
         appInfo.Category = 1;
         _mResult = ChromaAnimationAPI.InitSDK(ref appInfo);
         _mInitialized = true;
